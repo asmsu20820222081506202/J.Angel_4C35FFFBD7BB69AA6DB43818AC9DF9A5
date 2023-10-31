ca6d1f3f7ec403e63e7f86c0ef794d16dce1a691
asmsu20820222081506202 <144337129+asmsu20820222081506202@users.noreply.github.com>
2023-10-31 11:01:14 +0530

    Add files via upload

---
+++ challenege unit 1/main (1).py
@@ -0,0 +1,7 @@
+#challenge 2 : Check whether given year is leap year or not
+y=int(input("Enter valid year"))
+if(y%4==0):
+  print("Given year is a leap year")
+else:
+  print("Given year is not a leap year")
+
